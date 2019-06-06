f6aeb7970d7320853ad779135d3930f82d792b25
Arpita Chikkodi <apschikkodi14@gmail.com>
2019-06-06 16:43:36 +0530

    init

--- Fcfs&RR.c
+++ Fcfs&RR.c
@@ -1,105 +1,109 @@
 #include<stdio.h>
 #include<stdlib.h>
-
-int num,bt[20],at[20],wt[20],tat[20],rt[10];
-int i,temp=0,ct=0;
-float awt,atat;
-float sumWT,sumTAT;
-
-void takeInput(){
-    printf("\nEnter the number of process : ");
-    scanf("%d",&num);
-
-    for(i=0;i<num;i++){
-        printf("\nEnter execution time for process %d : ",i+1);
-        scanf("%d",&bt[i]);
-        printf("\nEnter arrival time for process %d : ",i+1);
-        scanf("%d",&at[i]);
-        rt[i]=bt[i]; 
-    }
-}
-int roundRobin(){
-    int count,j,time,remain,flag=0,tq; 
-    remain=num;  
-
-    printf("\nEnter Time Quantum:\t"); 
-  scanf("%d",&tq); 
-  printf("\n\nProcess\t|Turnaround Time|Waiting Time\n\n"); 
-  for(time=0,i=0;remain!=0;) 
-  { 
-    if(rt[i]<=tq && rt[i]>0) 
-    { 
-      time+=rt[i]; 
-      rt[i]=0; 
-      flag=1; 
-    } 
-    else if(rt[i]>0) 
-    { 
-      rt[i]-=tq; 
-      time+=tq; 
-    } 
-    if(rt[i]==0 && flag==1) 
-    { 
-      remain--; 
-      printf("P[%d]\t|\t%d\t|\t%d\n",i+1,time-at[i],time-at[i]-bt[i]); 
-      wt[i]+=time-at[i]-bt[i]; 
-      tat[i]+=time-at[i]; 
-      flag=0; 
-    }
-    if(i==num-1) 
-      i=0; 
-    else if(at[i+1]<=time) 
-      i++; 
-    else 
-      i=0; 
-  } 
-  printf("\nAverage Waiting Time= %1.3f",wt[i]*1.0/num); 
-  printf("\nAvg Turnaround Time = %1.3f",tat[i]*1.0/num); 
-  return 0; 
+int i,j,p[10]={1,2,3,4,5,6,7,8,9,10};
+int sum=0,at[10],rt[10],wt[10],bt[10],tt[10],ct=0;
+int npro;
+float tsum=0,wsum=0,tavg=0,wavg=0;
+void take()
+{
+	printf("\nEnter number of process(less than 10) : ");
+	scanf("%d",&npro);
+	for(i=0;i<npro;i++)
+	{
+		printf("\n\tEnter burst time of %d process : ",i+1);
+		scanf("%d",&bt[i]);
+		printf("\n\tEnter arrival time of %d process : ",i+1);
+		scanf("%d",&at[i]);
+		rt[i]=bt[i];
+	}
 }
 
-int fcfsMethod(){
-    wt[0]=0;  // take var temp and assign the first index value of arrival time,this helps to put process into ready queue
-    temp = at[0];  // this code of section also remove the fcfs overhead or fcfs convoy effect
-    for(i=1;i<num;i++){
-        temp=temp+bt[i-1];
-        wt[i]=temp-at[i];
-        sumWT=sumWT+wt[i];
-    }
-    printf("\nPid \t\t AT \t\t BT \t\t CT \t\t WT \t\t TAT");
-    for(i=0;i<num;i++){
-        ct += bt[i];
-        tat[i]=ct-at[i];
-        sumTAT += tat[i];
-        printf("\n%d \t\t %d \t\t %d \t\t %d \t\t %d \t\t %d\n",i+1,at[i],bt[i],ct,wt[i],tat[i]);
-    }
-    awt = sumWT/num;
-    atat = sumTAT/num;
-    printf("\nAverage turn around time : %f",atat);
-    printf("\nAverage waiting time : %f",awt);
-    printf("\n");
+void fcfs()
+{
+	wt[0]=0;
+	sum=at[0];
+	for(i=1;i<npro;i++)
+	{
+		sum=sum+bt[i-1];
+		wt[i]=sum-at[i];
+		wsum=wsum+wt[i];
+	}
+	
+	printf("\n\n****Result****");
+	printf("\nPID \t BT \t AT \t CT \t WT \t TAT");
+	for(i=0;i<npro;i++)
+	{
+		ct=ct+bt[i];
+		tt[i]=ct-at[i];
+		tsum=tsum+tt[i];
+		printf("\np%d \t %d\t %d\t %d\t %d\t %d",p[i],bt[i],at[i],ct,wt[i],tt[i]);
+	}
+	
+	wavg=wsum/npro;
+	tavg=tsum/npro;
+	printf("\nAverage waiting time is %1.3f",wavg);	
+	printf("\nAverage turn around time is %1.3f\n",tavg);
 }
-void main(){
-    printf("\nRound Robin & FCFS scheduling algorithm \n");
-    int ch;
-    while(1){
-        printf("\n1.Round Robin \n2.FCFS\n3.Exit\n");
-        printf("Enter choice : ");
-        scanf("%d",&ch);
-        switch (ch)
-        {
-            case 1:
-                 takeInput();
-                 roundRobin();
-                  break;
-            case 2:
-                 takeInput();
-                 fcfsMethod();
-                break;
-                
-            case 3: exit(0);
-            default: printf("\nInvalid choice!");
-                     break;
-        }
-    }
+
+int robin()
+{
+	int tq,time,remain,flag=0;
+	printf("\nEnter time quantun : ");
+	scanf("%d",&tq);
+	remain=npro;
+	printf("\n\nPID\tTurnAroundTime \t Waiting time\n\n");
+	for(time=0,i=0;remain!=0;)
+	{
+		if(rt[i]<=tq && rt[i]>0)
+		{
+			time+=rt[i];
+			rt[i]=0;
+			flag=1;
+		}
+		else if(rt[i]>0)
+		{
+			rt[i]-=tq;
+			time+=tq;
+		}
+		if(rt[i]==0 && flag==1)
+		{
+			remain--;
+			printf("\np[%d]\t\t %d \t\t %d",i+1,time-at[i],time-at[i]-bt[i]);
+			wt[i]=time-at[i]-bt[i];
+			tt[i]=time-at[i];
+			wsum+=wt[i];
+			tsum+=tt[i];
+			flag=0;
+		}
+		if(i==npro-1)
+			i=0;
+		else if(at[i+1]<=time)
+			i++;
+		else
+			i=0;
+	}
+	printf("\nAverage waiting time is %f",wsum/npro);
+	printf("\nAverage turn around time is %f",tsum/npro);
+	return 0;
+}
+
+void main()
+{
+	int ch;
+	while(1)
+	{
+		printf("\n1.FCFS \t2.RoundRobin\n");
+		printf("\nEnter choice : ");
+		scanf("%d",&ch);
+		switch(ch)
+		{
+			case 1: take();
+					fcfs();
+					break;
+					
+			case 2: take();
+					robin();
+					break;
+		}
+	}
 }
